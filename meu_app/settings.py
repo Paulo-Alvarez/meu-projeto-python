be04969f8c0f1af3f56f46793b008674f609516e
Paulo <paulozfilho2@gmail.com>
2025-06-05 10:52:38 -0300

    Adiciona domínio do Render ao ALLOWED_HOSTS

--- meu_app/settings.py
+++ meu_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ozxybadn2yks%hg)++nl#0if!kysews#g%-wc@%rl$vvi!#1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meu-projeto-python.onrender.com']
 
 
 # Application definition
